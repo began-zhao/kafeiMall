70b41810c3e2e9fbf8b0510671255751c835bccb
zhao zhigang <z-begun@outlook.com>
2022-08-17 07:56:01 +0000

    抽奖服务添加

---
+++ kafeimall-lottery/src/main/java/com/kafeimall/lottery/KafeimallLotteryApplication.java
@@ -0,0 +1,13 @@
+package com.kafeimall.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafeimallLotteryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafeimallLotteryApplication.class, args);
+    }
+
+}
---
+++ kafeimall-lottery/src/test/java/com/kafeimall/lottery/KafeimallLotteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafeimall.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafeimallLotteryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
