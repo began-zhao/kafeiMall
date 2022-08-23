02ec00aff204c36acf08436d14c218baffabe691
zhao zhigang <z-begun@outlook.com>
2022-08-23 07:38:18 +0000

    修改pom内容，支撑模块添加

---
+++ kafeimall-support/src/main/java/com/kafeimall/support/KafeimallSupportApplication.java
@@ -0,0 +1,14 @@
+package com.kafeimall.support;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ *
+ *
+ */
+public class KafeimallSupportApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(KafeimallSupportApplication.class, args);
+    }
+}
