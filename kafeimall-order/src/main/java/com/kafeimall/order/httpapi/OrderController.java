e352429b6ec30a42083f97de55eb2aaa0450ce9d
赵志刚 <834561898@qq.com>
2022-11-12 00:05:23 +0800

    修改部分内容

--- kafeimall-order/src/main/java/com/kafeimall/order/httpapi/OrderController.java
+++ kafeimall-order/src/main/java/com/kafeimall/order/httpapi/OrderController.java
@@ -1,7 +1,19 @@
 package com.kafeimall.order.httpapi;
 
+import com.kafeimall.common.result.Result;
+import com.kafeimall.order.application.OrderApplication;
+import com.kafeimall.order.application.dto.OrderConfirmDto;
+import com.kafeimall.order.httpapi.converter.OrderApiConverter;
+import com.kafeimall.order.httpapi.model.vo.OrderConfirmVo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @author: zzg
@@ -9,9 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description: 订单
  */
 @RestController
-@RequestMapping("order/")
+@RequestMapping("order")
 public class OrderController {
 
-//    @PostMapping
-//    public
+    @Resource
+    OrderApplication orderApplication;
+
+    @Resource
+    OrderApiConverter orderApiConverter;
+
+    /**
+     * 生成确认订单项
+     * @param cartIds 购物车ID
+     * @return OrderConfirmVo 确认订单项信息
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @PostMapping("/generateConfirmOrder")
+    public Result<OrderConfirmVo> generateConfirmOrder(@RequestBody @Valid List<Long> cartIds) throws ExecutionException, InterruptedException {
+        OrderConfirmDto orderConfirmDto = orderApplication.generateConfirmOrder(cartIds);
+        OrderConfirmVo orderConfirmVo = orderApiConverter.toOrderConfirmVo(orderConfirmDto);
+        return  Result.success(orderConfirmVo);
+    }
 }
