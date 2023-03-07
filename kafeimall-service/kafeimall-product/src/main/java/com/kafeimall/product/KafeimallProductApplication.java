63e6652102960f350f8dec89bf7a6f283db55048
赵志刚 <834561898@qq.com>
2023-03-07 23:13:21 +0800

    refactor(商品服务)：JPA全面替换mybatis
    1.项目部分结构调整，防腐层拆分,领域层变动

--- kafeimall-service/kafeimall-product/src/main/java/com/kafeimall/product/KafeimallProductApplication.java
+++ kafeimall-service/kafeimall-product/src/main/java/com/kafeimall/product/KafeimallProductApplication.java
@@ -1,6 +1,5 @@
 package com.kafeimall.product;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,7 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *
  */
 @SpringBootApplication
-@MapperScan("com.kafeimall.product.infrastructure.repo.dao")
 @EnableFeignClients(basePackages = "com.kafeimall.product.api.feign")
 @EnableDiscoveryClient
 public class KafeimallProductApplication {
